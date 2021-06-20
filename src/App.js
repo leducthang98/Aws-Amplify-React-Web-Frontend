b8862d29e85037c65b85256040b7536272c4129d
LeDucThang <leducthang98@gmail.com>
2021-06-20 22:47:11 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import Amplify, { API } from 'aws-amplify';
+import config from './aws-exports';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { useEffect, useState } from 'react';
+
+Amplify.configure(config)
 
 function App() {
+  const [petName, setPetName] = useState('')
+  const [petType, setPetType] = useState('')
+  const [pets, setPets] = useState([])
+
+  useEffect(() => {
+    API.get('petsapi', '/pets/name').then(petRes => {
+      setPets([...pets, ...petRes])
+    })
+  }, [])
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    API.post('petsapi', '/pets', {
+      body: {
+        name: petName,
+        type: petType
+      }
+    }).then(() => {
+      setPets([...pets, {
+        name: petName,
+        type: petType
+      }])
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit}>
+          <input value={petName} placeholder="name" onChange={(e) => setPetName(e.target.value)}></input>
+          <input value={petType} placeholder="type " onChange={(e) => setPetType(e.target.value)}></input>
+          <button>Add</button>
+        </form>
+        <ul>
+          {pets.map((pet) => {
+            return (
+              <li>{pet.name}</li>
+            )
+          })}
+        </ul>
+        <AmplifySignOut></AmplifySignOut>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
